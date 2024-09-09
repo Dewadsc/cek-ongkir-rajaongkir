001b17f7122e3579d1535eb11ff94595c5034b2a
Dewadsc <irawandewabuana@gmail.com>
2024-09-09 23:50:52 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,104 @@
+<a id="readme-top"></a>
+<br />
+<div align="center">
+  <h3 align="center">Website Cek Ongkir - RajaOngkir</h3>
+  <p align="center">
+    Website sederhana yang dibuat dan menggunakan API dari Raja Ongkir.
+    <br />
+    <a href="#">View Demo</a>
+    ·
+    <a href="https://github.com/othneildrew/Best-README-Template/issues/new?labels=bug&template=bug-report---.md">Report Bug</a>
+    ·
+    <a href="https://github.com/othneildrew/Best-README-Template/issues/new?labels=enhancement&template=feature-request---.md">Request Feature</a>
+  </p>
+</div>
+
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#using">Using</a></li>
+      </ul>
+    </li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
+
+## About The Project
+![Product Name Screen Shot][img001]
+
+Website ini saya buat sebenernya sebagai chalange saya untuk masuk kerja sebagai programmer, ya walaupun tidak lolos karna saya pelamar paling telat. Tapi tak pe karna chalange ini saya ada satu project sederhana yang bisa saya masukin ke github juga intinya :v
+
+Pada dasarnya website ini hanya merequest data ke Raja Ongkir dengan memanfaatkan API yang sudah di sediakan oleh Raja Ongkir. Data seperti apa? Ya sebenernya sudah jelas dari judul yak, yaitu untuk mengecek ongkir saja.
+
+Jadi website nya ya sesimpel itu, kita pilih pengiriman barang nya dari mana, terus tujuan nya kemana, beratnya berapa, dan juga jasa siapa yang ingin di pakai. API Raja Ongkir ini free, akan tetapi sangat terbatas.
+
+Jika anda memakai yang gratisan maka yang tersedia hanya JNE, Tiki, dan POS Indonesi. Tapi segitu aja udah bersyukur karna kita menggunakan yang gratis ya bre. Untuk tutorial gimana cara nya mendaftarkan serta menjalankan aplikasinya dari tahap awal sampai akhir, silahkan anda tonton video di chanel YouTube saya dengan cara klik `View Demo` di paling atas.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+### Built With
+Website ini dibangun hanya dengan menggunakan PHP dan juga JQuery untuk merequest API ke Raja Ongkir.
+
+* [![PHP][PHP.com]][PHP-url]
+* [![JQuery][JQuery.com]][JQuery-url]
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## Getting Started
+Sebelum anda menjalankan website ini, anda harus mendownload `xampp` terlebih dahulu di google, kemudian anda buat akun di Raja Ongkir, cek tutorial nya di chanel YouTube saya dengan cara klik `View Demo`, link ada di paling atas. Setelah sudah silahkan anda ikuti langkah langkah dibawah ini
+
+### Prerequisites
+Tidak banyak yang perlu di install, hanya `xampp` yang hanya perlu anda install pada laptop anda.
+
+### Using
+_Ketika `xampp` nya sudah di install langkah selanjutnya anda coba clone repository saya yang satu ini ya everybody. Cara nya gimana? intinya kalian ikuti langkah langkah dibawah ini ya bre._
+
+1. Clone repository
+   ```sh
+   git clone https://github.com/dewadsc/cek-ongkir-rajaongkir.git
+   ```
+2. Setelah sudah di clone silahkan anda pindahkan folder nya ke directory berikut
+   ```
+   C:\xampp\htdocs
+   ```
+3. Setelah sudah di pindahkan silahkan anda masuk ke dalam folder `app`
+4. Kemudian anda silahkan buka xampp dan anda jalankan dengan cara klik `Start` di `MySQL` dan juga `Apache` seperti gambar dibawah ini
+   ![Tutorial Menjalankan Xampp][tutor001]
+5. Setelah sudah anda silahkan buka browser dan buka url dibawah ini
+   ```
+   localhost/cek-ongkir-rajaongkir/app
+   ```
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## Contact
+Nama : Dewa Buana Irawan
+
+Sosial Media - [YouTube](https://www.youtube.com/@dstartup9210) - irawandewabuana@gmail.com - [Instagram](https://www.instagram.com/dewadsc1401/) - [WhatsApp](http://wa.me/6282284335177)
+
+Project Link: [https://github.com/dewadsc/cek-ongkir-rajaongkir](https://github.com/dewadsc/cek-ongkir-rajaongkir)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## Acknowledgments
+Terima kasih banyak buat kalian yang sudah mendownload, melaporkan error, atau yang sudah merequest fitur tambahan untuk program tersebut. Karna tanpa masukan atau respon dari kalian, maka program tersebut akan kurang berkembang.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+[tutor001]: images/image001.png
+[img001]: images/image002.png
+[JQuery.com]: https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white
+[JQuery-url]: https://jquery.com 
+[PHP.com]: https://img.shields.io/badge/php-%23777BB4.svg?style=for-the-badge&logo=php&logoColor=white
+[PHP-url]: https://www.php.net/
---
+++ app/index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="Description" content="Enter your description here" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <style type="text/css">
+      .preloader {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 9999;
+      }
+      .loading {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        font: 14px arial;
+      }
+    </style>
+    <title>API Cek Ongkir</title>
+  </head>
+  <body>
+    <div class="preloader">
+      <div class="loading">
+        <div class="spinner-border" role="status">
+        </div>
+        <br><br>
+        <span>Loading...</span>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col-lg-4">
+          <div class="card">
+            <div class="card-header">
+              <h3>Cek Ongkir</h3>
+            </div>
+            <div class="card-body">
+              <form id="form">
+                <div class="form-group">
+                  <label for="">Kota Asal</label>
+                  <select class="form-control select2" id="kota_asal" required></select>
+                </div>
+                <div class="form-group">
+                  <label for="">Kota Tujuan</label>
+                  <select class="form-control select2" id="kota_tujuan" required></select>
+                </div>
+                <div class="form-group">
+                  <label for="">Berat(gram)</label>
+                  <input type="number" class="form-control" id="berat" required/>
+                </div>
+                <div class="form-group">
+                  <label for="">Kurir</label>
+                  <select class="form-control" id="kurir" required>
+                    <option value="jne">JNE</option>
+                    <option value="tiki">TIKI</option>
+                    <option value="pos">POS INDONESIA</option>
+                  </select>
+                </div>
+                <button class="btn btn-sm btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-8">
+          <div class="card">
+            <div class="card-header">
+              <h3>Biaya Ongkir</h3>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-striped table-bordered">
+                  <thead>
+                    <tr>
+                      <th>No</th>
+                      <th>Layanan</th>
+                      <th>Deskripsi</th>
+                      <th>Harga</th>
+                      <th>Estimasi(Hari)</th>
+                    </tr>
+                  </thead>
+                  <tbody id="data_table"></tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+      $(document).ready(function () {
+        $(".select2").select2({
+          placeholder: "Pilih kota/kabupaten",
+          language: "id",
+        });
+        $.ajax({
+          type: "GET",
+          url: "kota.php",
+          success: function (res) {
+            $(".preloader").hide()
+            var data_option = "";
+            var data = JSON.parse(res);
+            data.rajaongkir.results.map((value) => {
+              data_option += `<option value="${value.city_id}">${value.type} ${value.city_name}</option>`;
+            });
+            $("#kota_asal").html(data_option);
+            $("#kota_tujuan").html(data_option);
+          },
+        });
+      });
+      form.onsubmit = (e) => {
+        e.preventDefault();
+        $(".preloader").show()
+        $.ajax({
+          type: "POST",
+          url: "cek_ongkir.php",
+          data: {
+            kota_asal: $("#kota_asal").val(),
+            kota_tujuan: $("#kota_tujuan").val(),
+            berat: $("#berat").val(),
+            kurir: $("#kurir").val(),
+          },
+          success: function (res) {
+            $(".preloader").hide()
+            var data_table = "";
+            var data = JSON.parse(res);
+            data.rajaongkir.results.map((value) => [
+              value.costs.map((value2, index) => {
+                data_table += `<tr>
+                        <td>${index + 1}</td>
+                        <td>${value2.service}</td> 
+                        <td>${value2.description}</td>  
+                        <td>Rp. ${Intl.NumberFormat().format(value2.cost[0]["value"])}</td> 
+                        <td>${value2.cost[0]["etd"]}</td> 
+                    </tr>`;
+              }),
+            ]);
+            $("#data_table").html(data_table);
+          },
+        });
+      };
+    </script>
+  </body>
+</html>
